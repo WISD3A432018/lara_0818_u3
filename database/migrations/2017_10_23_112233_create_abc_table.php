16f6529a6f24546acd606e97bbf6e8286a2ea126
img21326 <img21326@gmail.com>
2017-10-23 19:23:39 +0800

    新增數個 migration，並撰寫其內容新增數個 migration，並撰寫其內容3a432018

--- database/migrations/2017_10_23_112233_create_abc_table.php
+++ database/migrations/2017_10_23_112233_create_abc_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateAbcTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('abc', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('title');
-            $table->text('content');
+            $table->string('bbb');
+            $table->text('ccc');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('abc');
     }
 }
